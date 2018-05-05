7ca461239a1f6b227ac904a6e038f6c3c9a4eb01
Affected <affected700@yandex.ru>
2018-05-05 19:56:18 +0400

    Initial commit

---
+++ src/main/java/hello/HelloWorldController.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController
+{
+    @RequestMapping(method = RequestMethod.GET)
+    public String helloWorld()
+    {
+        return "Hello World22";
+    }
+}
